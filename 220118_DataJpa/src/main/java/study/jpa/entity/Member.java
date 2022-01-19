8197550332e81f5065bfb026cd79a5f0bdf24ae2
devboki <octsixxth@gmail.com>
2022-01-19 17:58:09 +0900

    Member/Team mapping, Member test

--- 220118_DataJpa/src/main/java/study/jpa/entity/Member.java
+++ 220118_DataJpa/src/main/java/study/jpa/entity/Member.java
@@ -1,22 +1,40 @@
 package study.jpa.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
+import org.hibernate.annotations.ManyToAny;
+
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"}) //연관관계 필드는 toString X
 public class Member {
 	
 	@Id @GeneratedValue
+	@Column(name = "member_id") //table명은 관례상 name 매핑
 	private Long id;
 	private String username;
+	private int age;
 	
-	protected Member() { //JPA는 파라미터가 없는 기본생성자 필요. private X 프록시가 동작할 때 방해받으므로
-	}
+	@ManyToOne(fetch = FetchType.LAZY) //ManyToOne 꼭 LAZY
+	@JoinColumn(name = "team_id") //Foreign key
+	private Team team;
+	
+	/*
+	protected Member() { } //JPA는 파라미터가 없는 기본생성자 필요. private X 프록시가 동작할 때 방해받으므로 
+	*/
 	
 	public Member(String username) { //setter 사용 X
 		this.username = username;
@@ -27,4 +45,17 @@ public class Member {
 		this.username = username;
 	}
 	*/
+	
+	public void changeTeam(Team team) {
+		this.team = team;
+		team.getMembers().add(this); //team의 member에 add
+	}
+
+	public Member(String username, int age, Team team) {
+		this.username = username;
+		this.age = age;
+		if (team != null) { //team이 null이어도 무시
+			changeTeam(team);
+		}
+	}
 }
