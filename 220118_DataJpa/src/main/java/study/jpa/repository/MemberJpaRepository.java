07182f2e03627780033d37ab725a7e2d1fbc43fb
devboki <octsixxth@gmail.com>
2022-01-20 12:37:17 +0900

    순수JPA CRUD + test code

--- 220118_DataJpa/src/main/java/study/jpa/repository/MemberJpaRepository.java
+++ 220118_DataJpa/src/main/java/study/jpa/repository/MemberJpaRepository.java
@@ -1,5 +1,8 @@
 package study.jpa.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
@@ -7,6 +10,7 @@ import org.springframework.stereotype.Repository;
 
 import study.jpa.entity.Member;
 
+//순수 JPA : CRUD (update X)
 @Repository
 public class MemberJpaRepository {
 
@@ -18,7 +22,29 @@ public class MemberJpaRepository {
 		return member;
 	}
 	
+	public void delete(Member member) {
+		em.remove(member);
+	}
+	
+	/* 전체 조회. JPQL */
+	public List<Member> findAll(){
+		return em.createQuery("select m from Member m", Member.class)
+				.getResultList();
+	}
+	
+	public Optional<Member> findById(Long id){
+		Member member = em.find(Member.class, id);
+		return Optional.ofNullable(member); //Java 8 Optional. member가 null일 수도 아닐 수도
+	}
+	
+	public long count() {
+		return em.createQuery("select count(m) from Member m", Long.class)
+				.getSingleResult(); //단건일 때는 single result
+	}
+	
+	/* 단건 조회. pk로 찍어서 */
 	public Member find(Long id) {
 		return em.find(Member.class, id);
 	}
+	
 }
--- 220118_DataJpa/src/test/java/study/jpa/repository/MemberJpaRepositoryTest.java
+++ 220118_DataJpa/src/test/java/study/jpa/repository/MemberJpaRepositoryTest.java
@@ -2,6 +2,8 @@ package study.jpa.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,7 +14,7 @@ import study.jpa.entity.Member;
 
 @SpringBootTest
 @Transactional //import org.springframework.transaction 
-@Rollback(false) //테스트시 sql log와 DB 확인 가능. 실무에서는 X
+@Rollback(false) //테스트시 sql log와 DB 확인 가능(commit 됨). 실무에서는 X
 public class MemberJpaRepositoryTest {
 
 	@Autowired MemberJpaRepository memberJpaRepository;
@@ -27,5 +29,39 @@ public class MemberJpaRepositoryTest {
 		assertThat(findMember.getId()).isEqualTo(member.getId());
 		assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
 		assertThat(findMember).isEqualTo(member); //1차 캐시 == 비교
+	}
+	
+	@Test
+	public void basicCRUD() {
+		Member member1 = new Member("member1");
+		Member member2 = new Member("member2");
+		memberJpaRepository.save(member1);
+		memberJpaRepository.save(member2);
+		
+		//단건 조회 검증
+		Member findMember1 = memberJpaRepository.findById(member1.getId()).get();
+		Member findMember2 = memberJpaRepository.findById(member2.getId()).get();
+		assertThat(findMember1).isEqualTo(member1);
+		assertThat(findMember2).isEqualTo(member2);
+		
+		/*
+		findMember1.setUsername("member!!!!!!"); 
+		//변경 감지. 더티 체킹 update member set age=0, team_id=NULL, username='member!!!!!!' where member_id=1;
+		*/
+		
+		//리스트 조회 검증
+		List<Member> all = memberJpaRepository.findAll();
+		assertThat(all.size()).isEqualTo(2);
+		
+		//카운트 검증
+		long count = memberJpaRepository.count();
+		assertThat(count).isEqualTo(2); //extracted value ([col_0_0_] : [BIGINT]) - [2]
+		
+		//삭제 검증
+ 		memberJpaRepository.delete(member1);
+ 		memberJpaRepository.delete(member2);
+		
+ 		long deletedCount = memberJpaRepository.count();
+		assertThat(deletedCount).isEqualTo(0); //extracted value ([col_0_0_] : [BIGINT]) - [0]
 	}
 }
